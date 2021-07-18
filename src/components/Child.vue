895498a623720acede49f632d6a1e5cfdfe0eb9f
BibekStha123 <bibekstha084@gmail.com>
2021-07-18 08:54:44 -0700

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <h3>{{ message }}</h3>
+        </div>
+        <div>
+            <button @click.prevent="emitToParent">From Child</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: String
+    },
+ 
+    methods:{
+        emitToParent() {//passing data to parent from child
+            this.$emit('childToParent', "To parent from child")
+        }
+    }
+}
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,20 @@
+<template>
+    <Child message="hellooo from parent" @childToParent="onChildClick"/>
+</template>
+
+<script>
+
+import Child from './Child.vue'
+
+export default {
+    components: {
+        Child
+    },
+    methods:{
+        onChildClick(value) {
+            alert(value);
+        }
+    }
+}
+
+</script>
